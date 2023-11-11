acba9f755fd8275a5e04f1a6453a32b18d57784d
MaitreStick <mastijaci99@gmail.com>
2023-11-10 23:13:06 -0500

    new view

---
+++ src/modules/adminpanel/views/Edit.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="d-flex justify-content-center" >
+          <h1>Esta es la sección del Edit</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
