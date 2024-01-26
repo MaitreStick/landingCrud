c171f3703ec134ca1fed55334fdfc482e5688174
MaitreStick <mastijaci99@gmail.com>
2024-01-26 09:56:17 -0500

    CreateEntryView View

---
+++ src/modules/adminpanel/views/CreateEntryView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="create-entry-view">
+      <createEntryform/>
+    </div>
+  </template>
+  
+  <script>
+  import { defineAsyncComponent } from 'vue';
+  
+  export default {
+    components: {
+        createEntryform: defineAsyncComponent(() => import('../components/CreateEntryForm.vue')),
+    }
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
