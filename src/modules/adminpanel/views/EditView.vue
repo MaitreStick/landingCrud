d431e9720f1da642585bf28afe42616ddae85c62
MaitreStick <mastijaci99@gmail.com>
2024-01-27 08:52:06 -0500

    EditView View

--- src/modules/adminpanel/views/EditView.vue
+++ src/modules/adminpanel/views/EditView.vue
@@ -1,21 +1,56 @@
 <template>
     <div class="col-4">
-        <Edit/>
+      <Edit :entry="entry" @update-entry="handleUpdateEntry"/>
     </div>
-</template>
-
-<script>
-import { defineAsyncComponent } from 'vue';
-
-
-export default {
+  </template>
+  
+  <script>
+  import { defineAsyncComponent } from 'vue';
+  import { mapState, mapActions } from 'vuex';
+  
+  export default {
+    props: {
+        id: {
+            type: String,
+            required: true
+        }
+    },
     components: {
-        Edit: defineAsyncComponent(() => import('../components/Edit.vue'))
+        Edit: defineAsyncComponent(() => import('../components/Edit.vue')),      
+  
+    },
+    data() {
+        return {
+            entry: null
+        };
+    },
+    computed: {
+        ...mapState('adminModule', ['entries'])
+    },
+    methods: {
+        ...mapActions('adminModule', ['loadEntries', 'updateEntry']),
+        loadEntry() {
+            if (this.entries && this.entries.length > 0) {
+                this.entry = this.entries.find(e => e.id === this.id);
+            } else {
+                this.loadEntries().then(() => {
+                    this.entry = this.entries.find(e => e.id === this.id);
+                });
+            }
+        },
+        handleUpdateEntry(updatedEntry) {
+          this.updateEntry(updatedEntry).then(() => {
+              // Aquí podrías redirigir al usuario o mostrar un mensaje de éxito
+              this.$router.push({ name: 'listview' }); // Por ejemplo, volver a la lista
+          }).catch(error => {
+              console.error('Error al actualizar:', error);
+              // Manejar el error (mostrar mensaje al usuario, etc.)
+          });
+        }
+    },
+    created() {
+        this.loadEntry();
     }
-
-}
-</script>
-
-<style>
-
-</style>
+  }
+  </script>
+  
