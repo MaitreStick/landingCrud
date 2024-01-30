732cd8392876bc98650d9b94c240144bf1033c34
MaitreStick <mastijaci99@gmail.com>
2024-01-30 07:24:24 -0500

    state Store adminmodule

--- src/modules/adminpanel/store/adminModule/state.js
+++ src/modules/adminpanel/store/adminModule/state.js
@@ -1,39 +1,7 @@
 
 export default () => ({
     isLoading: true,
-    entries: [ 
-        {
-            id: 1,
-            name: null,
-            document: null,
-            date: new Date().toDateString(),
-            phone: null,
-            pdf: null
-        },
-        {
-            id: 2,
-            name: null,
-            document: null,
-            date: new Date().toDateString(),
-            phone: null,
-            pdf: null
-        },
-        {
-            id: 3,
-            name: null,
-            document: null,
-            date: new Date().toDateString(),
-            phone: null,
-            pdf: null
-        },
-        {
-            id: 4,
-            name: null,
-            document: null,
-            date: new Date().toDateString(),
-            phone: null,
-            pdf: null
-        }
-     ]
+    entries: [],
+    selectedEntry: null
 })
 
