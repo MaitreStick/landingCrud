9d59efcf81b40ecd9a00cfbb4c8b9a05b1de9f96
MaitreStick <mastijaci99@gmail.com>
2024-01-31 08:49:20 -0500

    mutations store adminmodule

--- src/modules/adminpanel/store/adminModule/mutations.js
+++ src/modules/adminpanel/store/adminModule/mutations.js
@@ -1,20 +1,26 @@
+export const setEntries = (state, entries) => {
+    state.entries = entries;
+};
 
-// export const myMutation = ( state ) => {
-    
-// }
+export const selectEntry = (state, entryId) => {
+    state.selectedEntry = state.entries.find(entry => entry.id === entryId) || null;
+};
 
+export const updateEntry = (state, updatedEntry) => {
+    const index = state.entries.findIndex(entry => entry.id === updatedEntry.id);
+    if (index !== -1) {
+        state.entries.splice(index, 1, updatedEntry);
+    }
+};
 
-export const setEntries = ( /*state*/) => {
-    
-}
+export const addEntry = (state, newEntry) => {
+    state.entries.push(newEntry);
+};
 
-
-export const updateEntry = ( /*state*/) => {
-    
-}
-
-export const addEntry = ( /*state*/) => {
-    
-}
-
+export const removeEntry = (state, id) => {
+    const index = state.entries.findIndex(entry => entry.id === id);
+    if (index !== -1) {
+        state.entries.splice(index, 1);
+    }
+};
 
