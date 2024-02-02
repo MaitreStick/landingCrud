fa65c3e28b814b11f016153f8d094293d5a7862a
MaitreStick <mastijaci99@gmail.com>
2024-02-02 06:52:37 -0500

    actions store adminmodule

--- src/modules/adminpanel/store/adminModule/actions.js
+++ src/modules/adminpanel/store/adminModule/actions.js
@@ -1,18 +1,60 @@
+import adminApi from '@/api/adminApi';
 
-// export const myAction = async ({ commit }) => {
+export const loadEntries = async ({ commit }) => {
+    const { data } = await adminApi.get('/entries.json');
 
-// }
+    if (!data) {
+        commit('setEntries', []);
+        return;
+    }
 
-export const loadEntries = async (/*{ commit }*/) => {
+    const entries = [];
+    for (let id of Object.keys(data)) {
+        entries.push({
+            id,
+            ...data[id]
+        });
+    }
 
-}
+    commit('setEntries', entries);
+};
+
+export const updateEntry = async ({ commit }, updatedEntry) => {
+    const { id, ...data } = updatedEntry;
+    try {
+        await adminApi.put(`/entries/${id}.json`, data);
+        commit('updateEntry', updatedEntry);
+    } catch (error) {
+        console.error('Error al actualizar la entrada:', error);
+        throw error;
+    }
+};
 
 
-export const updateEntry = async (/*{ commit }*/) => {
+export const createEntry = async ({ commit }, entryData) => {
+    try {
+        const { data } = await adminApi.post('/entries.json', entryData);
+        const newEntry = { id: data.name, ...entryData };
 
-}
+        commit('addEntry', newEntry);
+
+        return data.name;
+    } catch (error) {
+        console.error('Error al crear la entrada:', error);
+        throw error;
+    }
+};
+
+export const deleteEntry = async ({ commit }, id) => {
+    try {
+        await adminApi.delete(`/entries/${id}.json`);
+        commit('removeEntry', id);
+    } catch (error) {
+        console.error('Error al eliminar la entrada:', error);
+        throw error;
+    }
+};
 
 
-export const createEntry = async (/*{ commit }*/) => {
 
-}
+
