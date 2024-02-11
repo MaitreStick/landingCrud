bf5c26df2c629179e71ff0ae6b6f0aa4a7baffba
MaitreStick <mastijaci99@gmail.com>
2024-02-10 22:09:00 -0500

    Navbar component adminpanel

--- src/modules/adminpanel/components/Navbar.vue
+++ src/modules/adminpanel/components/Navbar.vue
@@ -1,17 +1,52 @@
 <template>
-    <nav class="navbar bg-primary">
-        <a class="navbar-brand text-white">
+    <nav class="fixed top-0 left-0 w-full z-10 backgroundMenu px-4 py-2 flex justify-between items-center">
+        <a class="flex items-center text-2xl font-bold text-gray-100">
             <img src="@/assets/logo.png" 
-                 alt="Vue Logo"
-                 height="24"
-                 class="d-inline-block align-text-top mx-2">
+                 alt="Ace Logo"
+                 class="h-14 w-14 mr-2">
             Admin Panel
         </a>
 
+
         <div class="d-flex" >
-            <button class="btn btn-outline-info mx-2">
-                <i class="fa fa-sign-out-alt"></i>
+            <button @click="logout" class="text-white hover:text-gray-200">
+                <i class="fa fa-sign-out-alt"></i>Logout
             </button>
         </div>
     </nav>
+
 </template>
+
+
+<script>
+
+
+export default {
+    methods: {
+                   
+         logout() {
+            localStorage.removeItem('idToken');
+            localStorage.removeItem('refreshToken');
+            this.$store.commit('logout'); 
+            this.$router.push({ name: 'login' }).catch(err => {
+                console.log( err );
+            });
+        }
+    }
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+.backgroundMenu {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 100; 
+    border-radius: 0 0 10px 10px;
+    background: linear-gradient(245.59deg, #4d6995 0%, #385f70 28.53%, #133139 75.52%);
+}
+
+</style>
